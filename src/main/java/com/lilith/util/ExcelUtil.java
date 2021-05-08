d14b4ca639ca1c11a9a5985d18d31f43ad683b82
Jia-Jingnan <875480307@qq.com>
2021-05-09 00:40:18 +0800

    0508-datas()方法重载，传入数据为行号和列号集合，应用于读取Excel表中非连续的行列数据

--- src/main/java/com/lilith/util/ExcelUtil.java
+++ src/main/java/com/lilith/util/ExcelUtil.java
@@ -11,6 +11,7 @@ import java.io.File;
  */
 public class ExcelUtil {
 
+    // 读取连续行和列的数据
     public static Object[][] datas(String excelPath, int startRow, int endRow, int startCell, int endCell){
 
         // 存储数据的二维数组
@@ -44,10 +45,48 @@ public class ExcelUtil {
         return datas;
     }
 
-    // 验证数据是否取出
+    // 读取非连续的行和列的数据
+    public static Object[][] datas(String excelPath, int[] rows, int[] cells){
+
+        // 存储数据的二维数组
+        Object[][] datas = null;
+
+        try {
+            // 获取workbook对象
+            Workbook workbook = WorkbookFactory.create(new File(excelPath));
+            // 获取sheet对象
+            Sheet sheet = workbook.getSheet("用例");
+
+            // 定义保存提取出的数据的数组
+            datas = new Object[rows.length][cells.length];
+
+            // 获取行
+            // 通过循环获取每一行
+            for (int i = 0; i < rows.length; i++){
+                Row row = sheet.getRow(rows[i]);
+                // 获取列
+                // 第六列第七列为数据
+                for (int j = 0; j < cells.length; j++){
+                    Cell cell = row.getCell(cells[j]);
+                    // 将列设置为字符串类型
+                    cell.setCellType(CellType.STRING);
+                    String stringCellValue = cell.getStringCellValue();
+                    datas[i][j] = stringCellValue;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return datas;
+    }
+
+    // 验证传入行号列号集合是否取出数据
     public static void main(String[] args) {
         String excelPath = "src/main/resources/cases/cases.xls";
-        Object[][] datas = datas(excelPath,1,4,5,6);
+        int[] rows = {1,2,3,4};
+        int[] cells = {5,6};
+        Object[][] datas = datas(excelPath,rows,cells);
         for (Object[] data : datas) {
             for (Object datum : data) {
                 System.out.print("[" + datum + "]");
