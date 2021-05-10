c8ad6aa6a0ad57a61be923d0fab8c3b2deb4752a
Jia-Jingnan <875480307@qq.com>
2021-05-10 23:32:03 +0800

    0510-增加Case实体类，用来保存读取的用例信息

---
+++ src/main/java/com/lilith/entity/Case.java
@@ -0,0 +1,17 @@
+package com.lilith.entity;
+
+import lombok.Data;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午11:30 2021/5/10
+ * Case实体类，保存case信息
+ */
+@Data
+public class Case {
+
+    private String caseId;
+    private String apiId;
+    private String desc;
+    private String params;
+}
