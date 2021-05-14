5f2e4e169213450ecb31d0959b66cd00c5ce854b
Jia-Jingnan <875480307@qq.com>
2021-05-14 23:50:51 +0800

    0511-新增Api实体类，用来封装数据源中接口信息sheet页的数据

---
+++ src/main/java/com/lilith/entity/Api.java
@@ -0,0 +1,16 @@
+package com.lilith.entity;
+
+import lombok.Data;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午11:49 2021/5/14
+ */
+@Data
+public class Api {
+
+    private String apiId;
+    private String apiName;
+    private String type;
+    private String url;
+}
