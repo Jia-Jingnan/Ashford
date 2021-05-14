bca9da9e1406dacec4a1cc4c2bbacba568b6a79d
Jia-Jingnan <875480307@qq.com>
2021-05-15 00:07:17 +0800

    0514-根据load方法参数的修改，增加Case.class，表示将Case类型的对象加入到cases列表中

--- src/main/java/com/lilith/cases/RegisterCases.java
+++ src/main/java/com/lilith/cases/RegisterCases.java
@@ -2,6 +2,8 @@ package com.lilith.cases;
 
 import com.alibaba.fastjson.JSONObject;
 import com.lilith.entity.RegisterParam;
+import com.lilith.util.ApiUtil;
+import com.lilith.util.CaseUtil;
 import com.lilith.util.ExcelUtil;
 import com.lilith.util.HttpUtil;
 import org.testng.annotations.DataProvider;
@@ -30,26 +32,14 @@ public class RegisterCases {
      *         "password":"12345"
      */
 
-    @Test(dataProvider = "datas")
-    public void testRegister(String apiIdFromCase, String parameter){
-        // 获取接口信息sheet页内容,根据获取的接口编号获取接口地址
-        String url = null;
-        // 请求方式
-        String type = null;
-        int[] rows = {1,2,3,4};
-        int[] cells = {0,2,3};
-        String excelPath = "src/main/resources/cases/cases_v3.xlsx";
-        Object[][] datas = ExcelUtil.datas(excelPath,"接口信息",rows,cells);
-        for (Object[] data : datas) {
-            String apiIdFromApiInfo = data[0].toString();
-            // 接口编号一致时，取出url地址
-            if (apiIdFromCase.equals(apiIdFromApiInfo)){
-                type = data[1].toString();
-                url = data[2].toString();
-                break;
-            }
-        }
+    // 调用顺序
+    // Case(从数组中取出数据执行用例) -> CaseUtil(将对象中数据取出放在数组中) -> ExcelUtil(将数据封装成对象)
 
+    @Test(dataProvider = "datas")
+    public void testRegister(String apiId, String parameter){
+        // url
+        String url = ApiUtil.getUrlByApiId(apiId);
+        // type
 
         // 解析json格式字符串,将json格式字符串转换为Map，参数
         Map<String,String> params = (Map<String, String>) JSONObject.parse(parameter);
@@ -63,13 +53,12 @@ public class RegisterCases {
     // 使用poi解析Excel中的数据
     @DataProvider
     public Object[][] datas(){
-        String excelPath = "src/main/resources/cases/cases_v3.xlsx";
-
-        int[] rows = {1,2,3,4};
-        // 获取用例sheet页接口编号第3列，参数第4列
-        int[] cells = {2,3};
-        Object[][] datas = ExcelUtil.datas(excelPath,"用例", rows,cells);
+        String excelPath = "src/main/resources/cases/cases_v4.xlsx";
+        String[] cellNames = {"Params"};
+        Object[][] datas = CaseUtil.getCaseDatasByApiId("1",cellNames);
         return datas;
     }
+
+
 
 }
