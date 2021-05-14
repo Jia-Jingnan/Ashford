15ea6973ab798bd989c99053eab7537e27413d71
Jia-Jingnan <875480307@qq.com>
2021-05-15 00:17:16 +0800

    0514-增加ApiUtil，首先通过ExcelUtil中的load方法将接口信息sheet页的数据封装的Api对象中并将每一个对象加入apis列表中，然后通过判断对象列表中对象的apiId得到接口的url和type

---
+++ src/main/java/com/lilith/util/ApiUtil.java
@@ -0,0 +1,49 @@
+package com.lilith.util;
+
+import com.lilith.entity.Api;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 下午11:47 2021/5/14
+ */
+public class ApiUtil {
+
+    public static List<Api> apis = new ArrayList<>();
+
+    static {
+        ExcelUtil.load("src/main/resources/cases/cases_v4.xlsx","接口信息",Api.class);
+    }
+
+    /**
+     * 根据接口编号获取接口地址
+     * @param apiId
+     * @return
+     */
+    public static String getUrlByApiId(String apiId){
+        for (Api api : apis){
+            if (api.getApiId().equals(apiId)){
+                return api.getUrl();
+            }
+        }
+        return "";
+    }
+
+    // 根据apiId获取请求方式
+    public static String getTypeByApiId(String apiId){
+        for (Api api : apis){
+            if (api.getApiId().equals(apiId)){
+                return api.getType();
+            }
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        for (Api api : apis){
+            System.out.println(api);
+        }
+    }
+}
