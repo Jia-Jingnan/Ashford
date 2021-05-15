4221aeabd51349f29c4637e32825624a00195042
Jia-Jingnan <875480307@qq.com>
2021-05-15 12:20:46 +0800

    0515-增加一个基本测试类，封装基本请求处理过程，其他测试类继承这个BaseCase类

---
+++ src/main/java/com/lilith/cases/BaseCase.java
@@ -0,0 +1,38 @@
+package com.lilith.cases;
+
+import com.alibaba.fastjson.JSONObject;
+import com.lilith.entity.Result;
+import com.lilith.util.ApiUtil;
+import com.lilith.util.ExcelUtil;
+import com.lilith.util.HttpUtil;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+/**
+ * 基本Cases类，用来封装基本请求过程
+ * @Author:JiaJingnan
+ * @Date: 下午12:17 2021/5/15
+ */
+public class BaseCase {
+
+    @Test(dataProvider = "datas")
+    public void testRegister(String caseId, String apiId, String parameter){
+        // url
+        String url = ApiUtil.getUrlByApiId(apiId);
+        // type
+        String type = ApiUtil.getTypeByApiId(apiId);
+
+        // 解析json格式字符串,将json格式字符串转换为Map，参数
+        Map<String,String> params = (Map<String, String>) JSONObject.parse(parameter);
+
+        String response = HttpUtil.doService(type,url,params);
+
+        // 将响应结果保存在对象中
+        Result result = new Result("用例",caseId, "ActualResponseData",response);
+        // 对象保存到resultList中
+        ExcelUtil.resultList.add(result);
+
+        System.out.println(response);
+    }
+}
