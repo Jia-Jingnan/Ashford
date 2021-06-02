86b8d74a8e9c1a8df97327d8510bc6894cc44eb4
Jia-Jingnan <875480307@qq.com>
2021-06-02 13:06:01 +0800

    0602-增加DBChecker实体类，对应校验SQL语句，有两个属性no和sql

---
+++ src/main/java/com/lilith/entity/DBChecker.java
@@ -0,0 +1,14 @@
+package com.lilith.entity;
+
+import lombok.Data;
+
+/**
+ * @Author:JiaJingnan
+ * @Date: 21:51 2020/4/9
+ */
+@Data
+public class DBChecker {
+
+    private String no;
+    private String sql;
+}
